855404afcaca20c5041307ec3554fe1923cad3bd
jaw <62795911+JacobWestby@users.noreply.github.com>
2022-12-20 14:06:33 +0100

    backend works as intended. Added changestream and socketIO

--- src/server.js
+++ src/server.js
@@ -2,49 +2,113 @@ import express, { json } from 'express';
 import { db, connectToDb } from './db.js';
 import 'dotenv/config';
 import { ObjectId } from 'mongodb';
-
-
-// ADD ROOMS/CHATS TO MONGO DB AND GET THEM FROM THERE Done
-//  SETUP UP LOGIN OR AUTO COOKIE AND ID FOR USER Done
-
-// TODO: Add axios post request to frontend to post new chats to the DB
-// TODO: Add post request to create new room and add it to the DB
+import cors from 'cors';
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const httpServer = createServer(app);
+
+
+app.use(cors());
 app.use(express.json());
+
+// * Connect socketIO
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+// * Changestream for socketIO to update chat in real time
+
+io.on('connection', (socket) => {
+    const changeStream = db.collection('rooms').watch();
+    changeStream.on('change', (change) => {
+        try {
+            if (change.operationType === 'update') {
+                socket.emit('messageResponse', Object.values(change.updateDescription.updatedFields)[0]);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    })
+});
+
+// * Get all rooms
 
 app.get('/api/rooms', async (req, res) => {
     const room = await db.collection('rooms').find({}).toArray();
     res.send(room);
 });
 
-app.post('/api/rooms/:id', async (req, res) => {
-    const { name, message, chatID, id } = req.body;
+// * Get a single room
 
+app.get('/api/rooms/:id', async (req, res) => {
+    const roomId = ObjectId(req.params.id);
+
+    const room = await db.collection('rooms').findOne({ _id: roomId });
+    res.send(room);
+});
+
+// * Post a new chat to a room
+
+app.post('/api/rooms/:id', async (req, res) => {
+    const { name, message, chatID, id, roomID, time, socketID } = req.body;
     const roomId = ObjectId(req.params.id);
 
     await db.collection('rooms').updateOne({ _id: roomId }, {
-        $push: { chat: { name: name, message: message, chatID: chatID, id: id } }
+        $push: {
+            chat: {
+                name: name,
+                message: message,
+                chatID: chatID,
+                id: id,
+                roomID: roomID,
+                time: time,
+                socketID: socketID
+            }
+        }
     });
 
     const room = await db.collection('rooms').findOne({ _id: roomId });
-
-    console.log(req.body);
-    res.send(room.chat);
+    const chat = room.chat.find(chat => chat.id === id);
+    res.send(chat);
 });
 
-// app.get('/api/rooms/:currentRoom', async (req, res) => {
-//     const { currentRoom } = req.params;
-//     console.log(currentRoom);
-//     const room = await db.collection('rooms').findOne({ currentRoom });
-//     res.json(room);
-// });
+// * Create a new room
+
+app.post('/api/create', async (req, res) => {
+    const { name } = req.body;
+    const newRoom = await db.collection('rooms').insertOne({ name: name, chat: [] });
+
+    const room = await db.collection('rooms').findOne({ _id: newRoom.insertedId });
+    res.send(room);
+});
+
+
+// * Delete room 
+
+app.delete('/api/rooms/delete/:id', async (req, res) => {
+    const roomId = ObjectId(req.params.id);
+
+    try {
+        await db.collection('rooms').deleteOne({ _id: roomId });
+
+        res.send('Room deleted');
+    } catch (error) {
+        console.log(error);
+    };
+});
+
+// * =================== * //
 
 const PORT = process.env.PORT || 8000;
 
 connectToDb(() => {
     console.log('Successfully connected to database!');
-    app.listen(PORT, () => {
+    httpServer.listen(PORT, () => {
         console.log('Server is listening on port ' + PORT);
     });
 });
